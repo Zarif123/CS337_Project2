eb29a83f46509bb87fbeee2e7378444806b16b2c
Zarif Ceaser <zarifceaser321@gmail.com>
2023-11-27 14:09:39 -0600

    Adding url code

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,11 @@
 def view_list(recipe_item):
     for i in recipe_item:
         print(i)
+
+def build_url(query):
+    formatted_query = "+".join(query.split())
+    template = f"https://www.youtube.com/results?search_query={formatted_query}"
+    print(template)
+
+
+build_url("how to preheat an oven")
