2e62fa95a8c2421b0b42bedf496b5d39b18c0e7e
Zarif Ceaser <zarifceaser321@gmail.com>
2023-12-03 19:41:23 -0600

    Updated readme, zarif, haoyu

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Clone the repo
 Use `pip install -r requirements.txt` to install modules that were used for the project.
 Libraries used in the project are BeautifulSoup, requests, and re
 Running the main file in terminal using `python main.py` should run the project.
+All of the projects functionality can be used through the "5 - Step through the recipe" option.
+All natural language input that we accounted for such as "show me the ingredients" and "make it vegetarian" can be accessed through the step viewer ("5 - Step through the recipe")
--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
             print('\nI can help you transform the recipe!')
             print("You can change the serving size by saying: [halve, double, triple, quadruple, (any float)] the recipe")
             print("You can make the recipe vegetarian by saying: make it vegetarian")
-            print("You can make the recipe use Chinese ingredients by saying: make it Chinese")
+            print("You can make the recipe use Chinese ingredients by saying: make it Chinese, make it Mexican")
             transform_type = input("\nHow would you like to transform the recipe?\n")
             transform(transform_type)
         elif query == "7":
